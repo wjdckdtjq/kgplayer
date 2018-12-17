a397c72849ea7cae077f14b76dbc64217a3bb115
wjdckdtjq <44993292+wjdckdtjq@users.noreply.github.com>
2018-12-18 07:13:07 +0900

    Add files via upload

--- Jeoung/melon/MemberDAO.java
+++ Jeoung/melon/MemberDAO.java
@@ -1,10 +1,7 @@
-package melon;
+package application;
+
 
 import java.sql.*;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,7 +12,7 @@ public class MemberDAO {
 
 	
 	public void insert(MemberDTO dto) throws Exception {
-		String url = "jdbc:mysql://localhost:3306/kgplayer";
+		String url = "jdbc:mysql://localhost:3306/Kgplayer?useUnicode=true&characterEncoding=utf8";
 		String user = "root";
 		String password = "1234";
 
@@ -35,9 +32,54 @@ public class MemberDAO {
 		con.close();
 		ps.close();
 	}
+	public void insertSongList(String id) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer?useUnicode=true&characterEncoding=utf8";
+		String user = "root";
+		String password = "1234";
+
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "insert into memberSongList value(?,?)";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, id);
+		ps.setString(2, "5.mp3");
+		
+		ps.executeUpdate();
+		
+		con.close();
+		ps.close();
+	}
+	
+	
+	
+	public MemberSongList selectSongList(MemberSongList list) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer?useUnicode=true&characterEncoding=utf8";
+		String user = "root";
+		String password = "1234";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "select * from memberSongList where id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, list.getId());
+
+		ResultSet rs = ps.executeQuery();
+		MemberSongList list2 = null;
+		while (rs.next()) {
+			list2 = new MemberSongList();
+			list2.setId(rs.getString(1));
+			list2.setFileName(rs.getString(2));
+		}
+		
+		con.close();
+		ps.close();
+		
+		return list2;
+	}
+	
 
 	public MemberDTO select(String id) throws Exception {
-		String url = "jdbc:mysql://localhost:3306/kgplayer";
+		String url = "jdbc:mysql://localhost:3306/Kgplayer";
 		String user = "root";
 		String password = "1234";
 
@@ -64,9 +106,129 @@ public class MemberDAO {
 
 		return dto;
 	}
+	
+	public boolean idCheck(String id) throws Exception {
+		
+		String url = "jdbc:mysql://localhost:3306/Kgplayer";
+		String user = "root";
+		String password = "1234";
+
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "select count(id) from member where id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		int result = 0;
+		if(rs.next()) {
+			result = Integer.valueOf(rs.getString(1));
+		}
+		
+	
+
+		con.close();
+		ps.close();
+		rs.close();
+
+		return (result > 0) ? true : false;
+	}
+	
+	
+	public MemberDTO selectId(String id) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer";
+		String user = "root";
+		String password = "1234";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "select * from member where id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		MemberDTO dto = null;
+		while (rs.next()) {
+			dto = new MemberDTO();
+			dto.setId(rs.getString(1));
+			dto.setPw(rs.getString(2));
+			dto.setName(rs.getString(3));
+			dto.setTel(rs.getString(4));
+			dto.setAdress(rs.getString(5));
+			dto.setPoint(rs.getInt(6));
+			
+		}
+		
+		con.close();
+		ps.close();
+		rs.close();
+		return dto;
+	}
+	
+	public ArrayList<MemberDTO> selectinfo(String id) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer";
+		String user = "root";
+		String password = "1234";
+
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "select * from member where id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, id);
+		ResultSet rs = ps.executeQuery();
+		
+		ArrayList<MemberDTO> list = new ArrayList<MemberDTO>();
+		MemberDTO dto;
+		while (rs.next()) {
+			dto = new MemberDTO();
+			dto.setId(rs.getString(1));
+			dto.setPw(rs.getString(2));
+			dto.setName(rs.getString(3));
+			dto.setTel(rs.getString(4));
+			dto.setAdress(rs.getString(5));
+			dto.setPoint(rs.getInt(6));
+			list.add(dto);
+		}
+		con.close();
+		ps.close();
+		rs.close();
+
+		return list;
+	}
+	
+	
+	public MemberDTO selectName(String name) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer";
+		String user = "root";
+		String password = "1234";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "select * from member where name = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, name);
+		
+		ResultSet rs = ps.executeQuery();
+		MemberDTO dto = null;
+		while (rs.next()) {
+			dto = new MemberDTO();
+			dto.setId(rs.getString(1));
+			dto.setPw(rs.getString(2));
+			dto.setName(rs.getString(3));
+			dto.setTel(rs.getString(4));
+			
+		}
+		
+		con.close();
+		ps.close();
+		rs.close();
+		
+		return dto;
+	}
 
 	public ArrayList<MemberDTO> selectAll() throws Exception {
-		String url = "jdbc:mysql://localhost:3306/kgplayer";
+		String url = "jdbc:mysql://localhost:3306/Kgplayer";
 		String user = "root";
 		String password = "1234";
 
@@ -85,7 +247,7 @@ public class MemberDAO {
 			dto.setPw(rs.getString(2));
 			dto.setName(rs.getString(3));
 			dto.setTel(rs.getString(4));
-			dto.setAdress(rs.getString(5), rs.getString(5));
+			dto.setAdress(rs.getString(5));
 			dto.setPoint(rs.getInt(6));
 			list.add(dto);
 		}
@@ -96,7 +258,99 @@ public class MemberDAO {
 		return list;
 	}
 
+	public void delete(String id) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer";
+		String user = "root";
+		String password = "1234";
+
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "delete from member where id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, id);
+		ps.executeUpdate();
+		con.close();
+		ps.close();
+	}
+	
+	public void pointId(String id, int point) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer?useUnicode=true&characterEncoding=utf8";
+		String user = "root";
+		String password = "1234";
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "update member set point = point+? where id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, point);
+		ps.setString(2, id);
+		ps.executeUpdate();
+		con.close();
+		ps.close();
+		
+	}
+	
+	public void updateSongList(MemberSongList list) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer?useUnicode=true&characterEncoding=utf8";
+		String user = "root";
+		String password = "1234";
+
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "update memberSongList set songlist = concat(songList, ?) where id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		
+		ps.setString(1, "," + list.getFileName());
+		ps.setString(2, list.getId());
+
+		ps.executeUpdate();
+
+		con.close();
+		ps.close();
+	}
+	public void updateAllSongList(MemberSongList list) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer?useUnicode=true&characterEncoding=utf8";
+		String user = "root";
+		String password = "1234";
+
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "update memberSongList set songlist =? where id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		
+		ps.setString(1, list.getFileName());
+		ps.setString(2, list.getId());
+
+		ps.executeUpdate();
+
+		con.close();
+		ps.close();
+	}
+	
+	public void update(String id, String pw, String name, String tel, String adress) throws Exception {
+		String url = "jdbc:mysql://localhost:3306/Kgplayer?useUnicode=true&characterEncoding=utf8";
+		String user = "root";
+		String password = "1234";
+
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, user, password);
+		String sql = "update member set pw = ?, name = ?, tel = ?, adress = ? where id = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, pw);
+		ps.setString(2, name);
+		ps.setString(3, tel);
+		ps.setString(4, adress);
+		ps.setString(5, id);
+
+
+		ps.executeUpdate();
+
+		con.close();
+		ps.close();
+	}
+
 	
 
+	
 	
 }
--- Jeoung/melon/MemberJoin.java
+++ Jeoung/melon/MemberJoin.java
@@ -1,4 +1,5 @@
-package melon;
+package application;
+
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,9 +12,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.JToggleButton;
-
-import javafx.scene.control.ComboBox;
-
 import java.awt.Color;
 import java.awt.Checkbox;
 import javax.swing.ImageIcon;
@@ -25,113 +23,131 @@ public class MemberJoin extends JFrame {
 	private JTextField textField_2;
 	private JTextField textField_3;
 	private JTextField textField_4;
+	JButton btnNewButton_2;
 	String[] locate;
 	JComboBox comboBox;
-	String locate1 = "서울"; 
-	String locate2 = "인천"; 
-	String locate3 = "대구"; 
-	String locate4 = "부산"; 
-	String locate5 = "울산"; 
-	String locate6 = "제주도"; 
-	String locate7 = "강원도"; 
+	Robot r;
+	boolean confirm;
 	
 	public MemberJoin() {
-		setSize(500, 500);
+		getContentPane().setBackground(Color.WHITE);
+		
+		setSize(490, 648);
 		getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("\uC544\uC774\uB514");
-		lblNewLabel_1.setBounds(87, 108, 57, 15);
+		lblNewLabel_1.setBounds(87, 238, 57, 15);
 		getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("\uBE44\uBC00\uBC88\uD638");
-		lblNewLabel_2.setBounds(87, 153, 57, 15);
+		lblNewLabel_2.setBounds(87, 283, 57, 15);
 		getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("\uC774\uB984");
-		lblNewLabel_3.setBounds(87, 202, 57, 15);
+		lblNewLabel_3.setBounds(87, 335, 57, 15);
 		getContentPane().add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("\uC804\uD654\uBC88\uD638");
-		lblNewLabel_4.setBounds(87, 253, 57, 15);
+		lblNewLabel_4.setBounds(87, 383, 57, 15);
 		getContentPane().add(lblNewLabel_4);
 		
 		textField = new JTextField();
-		textField.setBounds(180, 105, 116, 21);
+		textField.setBounds(180, 232, 116, 21);
 		getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(180, 150, 116, 21);
+		textField_1.setBounds(180, 280, 116, 21);
 		getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(180, 199, 116, 21);
+		textField_2.setBounds(180, 329, 116, 21);
 		getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(180, 250, 116, 21);
+		textField_3.setBounds(180, 380, 116, 21);
 		getContentPane().add(textField_3);
 		textField_3.setColumns(10);
 		
-		JButton btnNewButton = new JButton("가입");
-		btnNewButton.setBounds(199, 366, 97, 36);
+		JButton btnNewButton = new JButton("\uD68C\uC6D0\uAC00\uC785");
+		btnNewButton.setBounds(192, 491, 97, 36);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				if (textField.getText().equals("")) {
-					JOptionPane.showMessageDialog(null,"회원정보를 입력하세요");	
-					
-				}else if(textField_1.getText().equals("")) {
-					JOptionPane.showMessageDialog(null,"회원정보를 입력하세요");	
-					
-				}else if(textField_2.getText().equals("")) {
-					JOptionPane.showMessageDialog(null,"회원정보를 입력하세요");	
-					
-				}else if(textField_3.getText().equals("")) {
-					JOptionPane.showMessageDialog(null,"회원정보를 입력하세요");	
-					
-				}else if(textField_4.getText().equals("")) {
-					JOptionPane.showMessageDialog(null,"회원정보를 입력하세요");	
+				MemberDAO dao = new MemberDAO();
+				try {
+					confirm = dao.idCheck(textField.getText());
+					System.out.println(confirm);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				if(btnNewButton_2.isEnabled() == true) {
+					JOptionPane.showMessageDialog(getContentPane(),"로봇확인해주세요");
 				
 				}else {
-					try {
-						int point = 100;
-						MemberDAO dao = new MemberDAO();
+					
+					if (textField.getText().equals("")) {
+						JOptionPane.showMessageDialog(getContentPane(),"회원정보를입력하세요");	
+						
+					}else if(textField_1.getText().equals("")) {
+						JOptionPane.showMessageDialog(getContentPane(),"회원정보를입력하세요");	
+						
+					}else if(textField_2.getText().equals("")) {
+						JOptionPane.showMessageDialog(getContentPane(),"회원정보를입력하세요");	
+						
+					}else if(textField_3.getText().equals("")) {
+						JOptionPane.showMessageDialog(getContentPane(),"회원정보를입력하세요");	
+						
+					}else if(textField_4.getText().equals("")) {
+						JOptionPane.showMessageDialog(getContentPane(),"회원정보를입력하세요");	
+						
+					}else if(confirm == true) { 
+						JOptionPane.showMessageDialog(getContentPane(),"아이디중복입니다");
+					}else if(confirm == false) {
 						MemberDTO dto = new MemberDTO();
-						dto.setId(textField.getText());
-						dto.setPw(textField_1.getText());
-						dto.setName(textField_2.getText());
-						dto.setTel(textField_3.getText());
-						String search = comboBox.getSelectedItem().toString();
-						dto.setAdress(search + textField_4.getText());
-						dto.setPoint(point);
-						dao.insert(dto);
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+						try {
+							int point = 100;
+//							MemberDAO dao = new MemberDAO();
+							dto.setId(textField.getText());
+							dto.setPw(textField_1.getText());
+							dto.setName(textField_2.getText());
+							dto.setTel(textField_3.getText());
+							String search = comboBox.getSelectedItem().toString();
+							dto.setAdress(search + " " +textField_4.getText());
+							dto.setPoint(point);
+							dao.insert(dto);
+							dao.insertSongList(textField.getText());
+						} catch (Exception e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						
+						
+						JOptionPane.showMessageDialog(getContentPane(),"회원가입완료");	
 					}
 					
-					JOptionPane.showMessageDialog(null,"가입완료");	
 				}
+				
+				
 				
 			}
 		});
 		getContentPane().add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("취소");
+		JButton btnNewButton_1 = new JButton("\uCDE8\uC18C");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				dispose();
 			}
 		});
-		btnNewButton_1.setBounds(305, 366, 103, 36);
+		btnNewButton_1.setBounds(303, 491, 103, 36);
 		getContentPane().add(btnNewButton_1);
 		
 		JLabel label = new JLabel("\uC8FC\uC18C");
-		label.setBounds(87, 305, 57, 15);
+		label.setBounds(87, 440, 57, 15);
 		getContentPane().add(label);
 		
 		textField_4 = new JTextField();
@@ -139,44 +155,82 @@ public class MemberJoin extends JFrame {
 		textField_4.setText("\uC0C1\uC138\uC8FC\uC18C");
 		textField_4.setToolTipText("\uC0C1\uC138\uC8FC\uC18C");
 		textField_4.setColumns(10);
-		textField_4.setBounds(205, 302, 154, 21);
+		textField_4.setBounds(213, 437, 154, 21);
 		getContentPane().add(textField_4);
 		
 		String[] locate = { "서울", "광주", "대구", "대전", "부산", "울산", "인천", "제주도", "울릉도", "강원도" };
 		comboBox = new JComboBox(locate);
-		comboBox.setBounds(131, 302, 62, 21);
+		comboBox.setBounds(139, 437, 62, 21);
 		getContentPane().add(comboBox);
 		
-		Checkbox checkbox = new Checkbox("");
-		checkbox.setBounds(148, 378, 12, 15);
-		getContentPane().add(checkbox);
-		
 		JLabel lblNewLabel_5 = new JLabel("New label");
-		lblNewLabel_5.setIcon(new ImageIcon("C:\\Users\\user\\Desktop\\\uCEA1\uCCD0\\025.jpg"));
-		lblNewLabel_5.setBounds(87, 25, 129, 62);
+		lblNewLabel_5.setIcon(new ImageIcon("C:\\Users\\user\\Desktop\\KakaoTalk_20181210_152723507.jpg"));
+		lblNewLabel_5.setBounds(19, 0, 453, 192);
 		getContentPane().add(lblNewLabel_5);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\user\\Desktop\\\uCEA1\uCCD0\\033.jpg"));
-		lblNewLabel.setBounds(181, 24, 190, 64);
-		getContentPane().add(lblNewLabel);
-		
-		JButton btnNewButton_2 = new JButton("로봇확인");
-		btnNewButton_2.setBounds(63, 368, 108, 33);
+		btnNewButton_2 = new JButton("\uB85C\uBD07\uD655\uC778");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				r = new Robot();
+//				System.out.println(r.confirm);
+				
+//				btnNewButton_2.setEnabled(false);
+//					try {
+//						r = new Robot();
+//						r.wait();	
+//						
+//					} catch (InterruptedException e1) {
+//						// TODO Auto-generated catch block
+//						e1.printStackTrace();
+//						
+//					}
+//				int confirm = dto.getConfirm();
+//				RobotDTO dto = new RobotDTO();
+//				}else if(r.confirm == 2) {
+//					btnNewButton_2.setEnabled(true);
+//				}
+				
+				dispose();
+				
+			}
+				
+
+		});
+		btnNewButton_2.setBounds(61, 493, 108, 33);
 		getContentPane().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("중복");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				MemberDAO dao = new MemberDAO();
+				try {
+					confirm = dao.idCheck(textField.getText());
+					System.out.println(confirm);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				if(confirm == true) {
+					JOptionPane.showMessageDialog(getContentPane(), "아이디중복");
+					
+				}else {
+					JOptionPane.showMessageDialog(getContentPane(), "사용가능 ");
+				}
+				
+			}
+		});
+		btnNewButton_3.setBounds(317, 230, 57, 23);
+		getContentPane().add(btnNewButton_3);
 		
 		
 		setVisible(true);
 	}
 
 	
-	
-	
-	
-	
-	
 	public static void main(String[] args) {
-		
-		MemberJoin mem = new MemberJoin();
+		MemberJoin join = new MemberJoin();
 	}
 }
